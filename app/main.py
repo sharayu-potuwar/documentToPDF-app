a95809c9a55b6f5785fb1313f7b0509a94f74156
Sharayu <sharayu@Sharayus-MacBook-Pro.local>
2025-01-20 15:50:02 -0500

    Dockerfile added

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 from spire.doc import *
 from spire.doc.common import *
-        
-# Create a Document object
-document = Document()
-# Load a Word DOCX file
-document.LoadFromFile("doc/demo.docx")
 
-# Or load a Word DOC file
-#document.LoadFromFile("Sample.doc")
+def convert(docfile):        
+    # Create a Document object
+    document = Document()
+    # Load a Word DOCX file
+    document.LoadFromFile("doc/${docfile}")
 
-# Save the file to a PDF file
-document.SaveToFile("pdf/WordToPdf.pdf", FileFormat.PDF)
-document.Close()
+    # Or load a Word DOC file
+    #document.LoadFromFile("Sample.doc")
+
+    # Save the file to a PDF file
+    document.SaveToFile("pdf/WordToPdf.pdf", FileFormat.PDF)
+    document.Close()
 
